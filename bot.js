d9dbd8fbca7892f2faf964af5aef6ca3dcff0642
medoookkk <48764289+medoookkk@users.noreply.github.com>
2019-03-20 18:49:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame(+help |By Medooo_96,"http://twitch.tv/DeathShop")
+ client.user.setGame(+help |Medooo_bot,"http://twitch.tv/DeathShop")
 });
 
 
